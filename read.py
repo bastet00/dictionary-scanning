ff50cfe7dbdf6a3d856fc752b95c30ab2a8d97b7
Yossef-Elshafey <yssef0472@gmail.com>
2024-09-02 05:54:21 +0300

    read logic

--- read.py
+++ read.py
@@ -1,78 +1,62 @@
 from pypdf import PdfReader
 
 
-class Cases:
-    @staticmethod
-    def ignore_new_lines(str: str):
-        return str.rstrip("\n").replace("\n", "")
-
-
-"""
-[
-    {Egyptian:[], Translation:[], Symbol:[]},
-    {Egyptian:[], Translation:[], Symbol:[]},
-    {Egyptian:[], Translation:[], Symbol:[]},
-    {Egyptian:[], Translation:[], Symbol:[]},
-    {Egyptian:[], Translation:[], Symbol:[]},
-    {Egyptian:[], Translation:[], Symbol:[]},
-]
-"""
-# TODO: the values needs to be looped over again to be ordered efficiently
-# TODO: generator functions might be the solving key
-
-
-class Read(Cases):
+class Read:
 
     def __init__(self, file):
         self.page = PdfReader(file).pages[0].extract_text()
         self.dto = []
 
-        self.egyptian()
-        # self.trans()
+        self.parse()
+
+    @staticmethod
+    def ignore_new_lines(str: str):
+        return str.rstrip("\n").replace("\n", "").strip()
 
     def search(self, char, start):
         return self.page.find(char, start)
 
-    def egyptian(self):
+    def get_egyptian(self, start):
+        start, end = self.search("[", start), self.search("]", start + 1)
+        return (start, end)
+
+    def get_translation(self, start):
+        start, end = self.search("]", start), self.search("{", start + 1)
+        return (start, end)
+
+    def get_symbol(self, start):
+        start, end = self.search("{", start), self.search("}", start + 1)
+        return (start, end)
+
+    def parse(self):
         line_start = 0
-        for i in range(len(self.page)):
-            start, end = self.search("[", line_start), self.search("]", line_start + 1)
-            start2, end2 = self.search("]", line_start), self.search(
-                "{", line_start + 1
-            )
-            if start == -1:
+        for i in range(5):
+            egy = self.get_egyptian(line_start)
+            trans = self.get_translation(line_start)
+            symbol = self.get_symbol(line_start)
+
+            if symbol[1] < trans[1]:
+                symbol = self.get_symbol(symbol[0])
+
+            if egy[0] == -1:
                 break
-            line_start = end
-            self.parse("Egyptian", self.page[start + 1 : end])
-            self.parse(
-                "Translation",
-                self.ignore_new_lines(self.page[start2 + 1 : end2]).split(","),
+            line_start = max(egy + trans + symbol)
+
+            self.object_append(
+                self.page[egy[0] + 1 : egy[1]],
+                self.page[trans[0] + 1 : trans[1]],
+                self.page[symbol[0] + 1 : symbol[1]],
             )
 
-    # def trans(self):
-    #     line_start = 0
-    #     for i in range(len(self.page)):
-    #         start, end = self.search("]", line_start), self.search("{", line_start + 1)
-    #         if start == -1:
-    #             break
-    #         line_start = end
-    #         self.parse(
-    #             "Translation",
-    #             self.ignore_new_lines(self.page[start + 1 : end]).split(","),
-    #         )
-
-    def parse(self, key, value):
-        self.dto.append({key: value})
+    def object_append(self, *args):
+        self.dto.append(
+            {
+                "Egyptian": args[0],
+                "Translation": self.ignore_new_lines(args[1]).split(", "),
+                "Symbol": self.ignore_new_lines(args[2]).split(" "),
+            }
+        )
 
 
 read = Read("DictionaryOfMiddleEgyptian-12-13.pdf")
 print(read.dto)
-# eg = 0
-# trans = 0
-# for i in read.dto:
-#     if "Egyptian" in i.keys():
-#         eg += 1
-#     else:
-#         trans += 1
-#
-# print(eg, trans)
